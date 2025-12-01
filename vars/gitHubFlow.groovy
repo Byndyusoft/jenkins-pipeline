e061e5a50027603868c5f772b3a5a276b2e9f764
Yefremov Mikhail <emsmihail@gmail.com>
2025-12-01 13:47:16 +0300

    bumped version to 1.0.4

--- vars/gitHubFlow.groovy
+++ vars/gitHubFlow.groovy
@@ -14,7 +14,7 @@ def call(Map serviceSetting = [:], List<String> checks = [], Map k8sCloud = [:],
 
     jenkinsFileSettings.initialize(serviceSetting)
 
-    final String pipelineVersion = '1.0.3'
+    final String pipelineVersion = '1.0.4'
     final String configDir = './deploy'
 
     logger.logInfo('###################################################################')
