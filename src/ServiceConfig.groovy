9953e0270e037329cb8d625501c8b9ce1daed27f
Maxim Nikitin <nikitin@byndyusoft.com>
2025-12-30 20:03:40 +0500

    Testing of ideas

--- src/ServiceConfig.groovy
+++ src/ServiceConfig.groovy
@@ -6,7 +6,7 @@ class ServiceConfig {
     String makeFileEnvString
     String helmOption
 
-    ServiceConfig (Yaml serviceYaml) {
+    ServiceConfig(Yaml serviceYaml) {
         microservice = serviceYaml.get('microservice') as Map ?: [:]
 
         Utils utils = new Utils()
