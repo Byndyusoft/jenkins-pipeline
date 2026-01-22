56220be7ac3af9ffd42865fc73a692220529ce66
Maxim Nikitin <nikitin@byndyusoft.com>
2026-01-22 17:46:45 +0500

    Testing of ideas

--- src/CommonConfig.groovy
+++ src/CommonConfig.groovy
@@ -9,12 +9,12 @@ class CommonConfig {
     void initialize(Yaml commonYaml) {
         Utils utils = new Utils()
 
-        common = serviceYaml.get('common') as Map ?: [:]
+        common = commonYaml.get('common') as Map ?: [:]
 
-        makeOption = serviceYaml.get('make/option') ?: ''
-        makeFileEnv = serviceYaml.get('makefile/env') as Map ?: [:]
+        makeOption = commonYaml.get('make/option') ?: ''
+        makeFileEnv = commonYaml.get('makefile/env') as Map ?: [:]
         makeFileEnvString = utils.mapToString(makeFileEnv) ?: ''
 
-        helmOption = serviceYaml.get('helm/option') ?: ''
+        helmOption = commonYaml.get('helm/option') ?: ''
     }
 }
