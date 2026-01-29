446b9882371b2886cb1bc612e3197ec38acae336
Maxim Nikitin <niki91m@yandex.ru>
2026-01-29 14:35:13 +0500

    Testing of ideas

--- vars/gitHubFlow.groovy
+++ vars/gitHubFlow.groovy
@@ -65,7 +65,7 @@ def call() {
 
                         List<ArtifactType> artifactTypes = utils.mapArtifactType(serviceConfig.artifactSetting.get('type') as List<String> ?: [])
 
-                        artifactsTypes.addAll([artifactTypes])
+                        artifactsTypes.addAll([artifactTypes.flatten()])
 
                         artifactsVariables.put("${microserviceName}", [
                             "artifactTypes": artifactTypes,
