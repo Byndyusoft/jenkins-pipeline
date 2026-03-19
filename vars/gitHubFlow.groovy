836c76c9f925df324b50d297100d83ccfb9670e8
Maxim Nikitin <niki91m@yandex.ru>
2026-03-19 11:50:50 +0500

    Testing skip git indexed

--- vars/gitHubFlow.groovy
+++ vars/gitHubFlow.groovy
@@ -20,15 +20,17 @@ def call() {
     logger.logInfo("Debug mode is env.DEBUG=${environmentVariables.DEBUG}")
     logger.logInfo('###################################################################')
 
-    def causes = currentBuild.rawBuild.getCauses()
+    script {
+        def causes = currentBuild.rawBuild.getCauses()
 
-    def isGitIndexed = causes.any { cause ->
-        cause instanceof hudson.triggers.SCMTrigger.SCMTriggerCause
-    }
+        def isGitIndexed = causes.any { cause ->
+            cause instanceof hudson.triggers.SCMTrigger.SCMTriggerCause
+        }
 
-    if (environmentVariables.BUILD_NUMBER == '1' || isGitIndexed) {
-        logger.logInfo('Pipeline parameters updated, ignore build, exit from pipeline')
-        return
+        if (environmentVariables.BUILD_NUMBER == '1' || isGitIndexed) {
+            logger.logInfo('Pipeline parameters updated, ignore build, exit from pipeline')
+            return
+        }
     }
 
     Kubernetes kubernetes = new Kubernetes(this)
