6dc25981fd343c0805e84df7cd6d5dbcc548ebb4
Maxim Nikitin <niki91m@yandex.ru>
2026-07-08 16:54:22 +0500

    Testing

--- src/PipelineParameters.groovy
+++ src/PipelineParameters.groovy
@@ -63,8 +63,8 @@ class PipelineParameters {
             logger.logDebug("PipelineParameters:initialize deployCluster = ${deployCluster}")
 
             logger.logDebug("PipelineParameters:initialize deployEnvironment = ${deployEnvironment}")
-            logger.logDebug("PipelineParameters:initialize test = ${deployConfig.clouds?.(deployCluster)?.environment?.contains(deployEnvironment)}")
-            if (deployConfig.clouds?.(deployCluster)?.environment?.contains(deployEnvironment)) {
+            logger.logDebug("PipelineParameters:initialize test = ${deployConfig.clouds?.get(deployCluster)?.environment?.contains(deployEnvironment)}")
+            if (deployConfig.clouds?.get(deployCluster)?.environment?.contains(deployEnvironment)) {
                 cluster = deployCluster
                 logger.logDebug("PipelineParameters:initialize cluster = ${cluster}")
                 break
