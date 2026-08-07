b76f4ed86f7f580e9a0ed20897521cdc5b64a893
Maxim Nikitin <niki91m@yandex.ru>
2026-08-07 18:17:02 +0500

    Testing

--- vars/Nelm.groovy
+++ vars/Nelm.groovy
@@ -12,7 +12,7 @@ class Nelm {
 
     void deployApplication(DeployConfig deployConfig, CommonConfig commonConfig, ArtifactCommonSettings artifactCommonSettings, EnvironmentVariables environmentVariables) {
         try {
-            unstash 'valuesFile'
+            script.unstash 'valuesFile'
 
             script.sh("""nelm release install --auto-rollback \
                         ${(environmentVariables.DEBUG ? '--log-level="debug"' : '')} \
@@ -66,6 +66,6 @@ class Nelm {
 
         fullValues.microservices.add(utils.merge(valuesOverrides, valuesOverridesSecret))
         script.writeYaml file: deployConfig.microServiceValuesFilePath, overwrite: true, data: fullValues
-        stash name: 'valuesFile', includes: "${deployConfig.defaultValuesFilePath},${deployConfig.microServiceValuesFilePath}"
+        script.stash name: 'valuesFile', includes: "${deployConfig.defaultValuesFilePath},${deployConfig.microServiceValuesFilePath}"
     }
 }
